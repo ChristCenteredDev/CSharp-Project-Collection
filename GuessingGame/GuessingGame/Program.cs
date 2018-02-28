45fad6241b0aa6ae2e36cf3415683abedfc8730f
William M <wmooney1984@gmail.com>
2018-02-28 12:57:32 -0500

    Collection - Guessing Game - Completed

--- GuessingGame/GuessingGame/Program.cs
+++ GuessingGame/GuessingGame/Program.cs
@@ -6,6 +6,79 @@ namespace GuessingGame
   {
     static void Main(string[] args)
     {
+      // Display's App Info
+      GetAppInfo();
+
+      // Ask for Users Name and Get Info
+      GreetUser();  
+
+      while (true)
+      {
+
+        // Create A Random Object
+        Random random = new Random();
+
+        // Init Correct Number
+        int correctNumber = random.Next(1, 11);
+
+        // Initial Guess Variable
+        int guess = 0;
+
+        // Ask User For Number
+        Console.WriteLine("Guess a number between 1 and 10");
+
+        // While Guess Is Not Correct
+        while (guess != correctNumber)
+        {
+          // Get User Input
+          string input = Console.ReadLine();
+
+          // Make Sure Its A Number
+          if (!int.TryParse(input, out guess))
+          {
+            // Print Error Message
+            PrintColorMessage(ConsoleColor.Red, "That is not a number, please try again...");
+
+            // Keep Going
+            continue;
+          }
+
+          // Cast To Int And Put In Guess
+          guess = Int32.Parse(input);
+
+          // Match Guess To Correct Number
+          if (guess != correctNumber)
+          {
+            // Print Error Message
+            PrintColorMessage(ConsoleColor.Red, "Wrong number, please try again...");
+
+          }
+        }
+
+        // Print Success Message
+        PrintColorMessage(ConsoleColor.Yellow, "That is correct!!!");
+
+        // Play Again?
+        Console.WriteLine("Do you want to play again? [Y or N]");
+
+        // Get Answer
+        string answer = Console.ReadLine().ToUpper();
+
+        if (answer == "Y")
+        {
+          continue;
+        }
+        else
+        {
+          return;
+        }
+
+      }
+      
+    }
+
+    static void GetAppInfo()
+    {
       // Application Variables
       string appName = "Guessing Game";
       string appVersion = "1.0.0";
@@ -19,7 +92,11 @@ namespace GuessingGame
 
       // Reset Text Color
       Console.ResetColor();
+    }
 
+    // Get and display user info
+    static void GreetUser()
+    {
       // Ask Users Name
       Console.WriteLine("What is your name?");
 
@@ -27,70 +104,20 @@ namespace GuessingGame
       string inputName = Console.ReadLine();
 
       Console.WriteLine("Hello {0}, let's play a game...", inputName);
+    }
 
-      // Create A Random Object
-      Random random = new Random();
-
-      // Init Correct Number
-      int correctNumber = random.Next(1, 11);
-
-      // Initial Guess Variable
-      int guess = 0;
-
-      // Ask User For Number
-      Console.WriteLine("Guess a number between 1 and 10");
-
-      // While Guess Is Not Correct
-      while (guess != correctNumber)
-      {
-        // Get User Input
-        string input = Console.ReadLine();
-
-        // Make Sure Its A Number
-        if (!int.TryParse(input, out guess))
-        {
-          // Change Text Color
-          Console.ForegroundColor = ConsoleColor.Red;
-
-          // Tell User It's Not A Number
-          Console.WriteLine("It's not a number, please try again...");
-
-          // Reset Text Color
-          Console.ResetColor();
-
-          // Keep Going
-          continue;
-        }
-
-        // Cast To Int And Put In Guess
-        guess = Int32.Parse(input);
-
-        // Match Guess To Correct Number
-        if (guess != correctNumber)
-        {
-          // Change Text Color
-          Console.ForegroundColor = ConsoleColor.Red;
-
-          // Tell User It's The Wrong Number
-          Console.WriteLine("Wrong number, please try again...");
-
-          // Reset Text Color
-          Console.ResetColor();
-
-        }
-      }
-
+    // Print Color Message
+    static void PrintColorMessage(ConsoleColor color, string message)
+    {
       // Change Text Color
-      Console.ForegroundColor = ConsoleColor.Yellow;
+      Console.ForegroundColor = color;
 
-      // Tell User It's The Correct Number
-      Console.WriteLine("That is correct, do you want to play again?");
+      // Tell User It's Not A Number
+      Console.WriteLine(message);
 
       // Reset Text Color
       Console.ResetColor();
-
-
-      Console.ReadLine();  // Pauses the Console to read it...
     }
+
   }
 }
